3fe1b979b52a070a0ed2dcf61d8a66165eb571b0
fengweihuan <1004743308@qq.com>
2019-10-29 14:18:47 +0800

    project init

---
+++ src/views/home/index.vue
@@ -0,0 +1,36 @@
+<!-- home -->
+<template lang="pug">
+  .home
+    p 首页
+    van-button(type="primary") 主要按钮
+</template>
+
+<script>
+// import { api } from '@/config'
+import {
+  Button
+} from 'vant'
+export default {
+  components: {
+    'van-button': Button
+  },
+
+  data () {
+    return {}
+  },
+
+  computed: {},
+
+  mounted () {
+    console.log(process.env)
+  },
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+p {
+  width: 375px;
+}
+</style>
